bd607e052e0f35930f8393dc9f914faa45941ae5
Martim Lobao <6430786+martimlobao@users.noreply.github.com>
2018-07-11 14:30:08 -0400

    add comment to problem 5

--- 005/005.py
+++ 005/005.py
@@ -2,6 +2,7 @@ from functools import reduce
 
 
 def primes_up_to(n):
+    """Implementation of the sieve of Eratosthenes"""
     if n < 2:
         return []
     elif n < 3:
@@ -20,6 +21,16 @@ def primes_up_to(n):
 
 
 def prob005():
+    """
+    All primes up to max must divide the smallest multiple, and so are
+    added to the list. For every power of a prime p up to max, we also
+    add p again to the list. Now, for every composite number less than
+    max, its prime factors were either added to the list in the first
+    step (if the prime factor appears only once in the factorization)
+    or they were added at least k times to the list in the first and
+    second steps, where k is the number of times a given prime appears
+    in the factorization.
+    """
     max = 20
     factors = primes_up_to(max)
     extrafactors = []
